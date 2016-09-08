d6fe5de8e12f43b5b449f8ecf53c3d7cbfcb8c04
syafiq <Syafiq,rezpector@gmail.com>
2016-09-08 15:05:07 +0700

    Initial commit

---
+++ app/src/main/java/dev/co/syafiq/app1parent/MainActivity.java
@@ -0,0 +1,15 @@
+package dev.co.syafiq.app1parent;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
